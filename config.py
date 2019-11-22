6a64d28eab8195f9b643d95838df866b1bcd0461
Cristian Alvarez H <alvarezhernandezcristian1@gmail.com>
2019-11-22 17:01:22 -0500

    Add function to upload to AWS S3

--- config.py
+++ config.py
@@ -9,3 +9,10 @@ credenciales = {
     'usuario': 'root',
     'pass': ''
 }
+
+space = {
+    'aws_access_key_id': 'aws_access_key_id',
+    'aws_secret_access_key': 'aws_secret_access_key',
+    'endpoint_url': 'endpoint_url',
+    'region_name': 'region_name',
+}
--- main.py
+++ main.py
@@ -1,17 +1,53 @@
 from os import listdir, remove, popen
 from datetime import datetime
-from config import credenciales, bases_de_datos
+from config import credenciales, bases_de_datos, space
 import logging
 import subprocess
+import boto3
 
 logging.basicConfig(
     filename='logs/logs.txt',
     filemode='a',
-    level=logging.DEBUG,
+    level=logging.INFO,
     format='%(asctime)s > %(levelname)s : %(message)s',
     datefmt='%y-%m-%d %H:%M:%S'
 )
 
+def enviarBackupsComprimidosConBoto3():
+    logging.info('[INICIA] ENVIO DE LOS BACKUPS COMPRIMIDOS')
+
+    listaComprimidos = listdir('backups_comprimidos/')
+
+    for archivo in listaComprimidos:
+        sessionS3 = boto3.session.Session()
+
+        s3Client = sessionS3.client(
+            service_name='s3',
+            region_name= space['region_name'],
+            aws_access_key_id= space['aws_access_key_id'],
+            aws_secret_access_key= space['aws_secret_access_key'],
+            endpoint_url= space['endpoint_url']
+        )
+
+        try:
+
+            logging.info('Enviamos archivo:    %s' % archivo)
+
+            with open('backups_comprimidos/'+archivo, 'rb') as data:
+                s3Client.upload_fileobj(
+                    data,  # Archivo
+                    'copiasdeseguridad',  # Nombre del Space
+                    archivo #  Nombre del archivo en el Space cuando suba
+                )
+
+            logging.info('Borramos archivo:    %s' % archivo)
+
+            remove('backups_comprimidos/' + archivo)  # Borramos el archivo una vez enviado
+
+        except Exception as e:
+            logging.error('Error al enviar el archivo %s' % archivo)
+
+    logging.info('[FIN] ENVIO DE LOS BACKUPS COMPRIMIDOS')
 
 def comprimirBackups():
     logging.info('[INICIA] COMPRESIÓN DE LOS ARCHIVOS DE BACKUPS')
@@ -27,7 +63,7 @@ def comprimirBackups():
         #cmd = 'tar zcf %s %s' % ('backups_comprimidos/'+nombreBackupComprimido, 'backups/'+backup)
         cmd = ' tar -c %s | bzip2 > %s' % ('backups/'+backup, 'backups_comprimidos/'+nombreBackupComprimido) # Compresion mas fuerte
 
-        logging.info('Comprimimos: %s  >  %s' % (backup, nombreBackupComprimido) )
+        logging.info('Comprimimos:   %s  >  %s' % (backup, nombreBackupComprimido) )
 
         try:
             proceso = subprocess.Popen(cmd, shell=True)
@@ -35,12 +71,13 @@ def comprimirBackups():
         except Exception as e:
             logging.error('Al ejecutar comando para comprimir, Error: %s' % e)
 
-        logging.info('Borramos: %s' % backup)
+        logging.info('Borramos:      %s' % backup)
 
         remove('backups/'+backup) # Borramos el archivo original despues de estar comprimido
 
 
     logging.info('[TERMINA] COMPRESIÓN DE LOS ARCHIVOS DE BACKUPS')
+    logging.info('')
 
 
 def crearBackup(db):
@@ -70,6 +107,8 @@ def main():
         crearBackup(db)
 
     comprimirBackups()
+
+    enviarBackupsComprimidosConBoto3()
 
     logging.info(' --------------------------- PROCESO TERMINADO -----------------------------')
     logging.info(' ===========================================================================')
